b0fd5787ed10f194b38d9d211e96cbcaa21d7e67
Felix Descoteaux <fdescoteaux@LMM122.local>
2016-06-20 14:28:36 -0400

    Improved tests

--- {{cookiecutter.package_name}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.package_name}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -1,12 +1,33 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from subprocess import check_output
+import os
+import pytest
+import tempfile
+from subprocess import check_output, CalledProcessError
 
 
-def test_echo():
-    """An example test."""
-    result = run_cmd("echo hello world")
-    assert result == "hello world\n"
+@pytest.fixture(scope='module')
+def temp_file(request):
+    temp_file = open(tempfile.NamedTemporaryFile().name, 'w+')
+
+    def fin():
+        os.unlink(temp_file.name)
+        temp_file.close()
+
+    request.addfinalizer(fin)
+    return temp_file
+
+
+def test_script_raises_if_no_arg_given():
+    with pytest.raises(CalledProcessError):
+        run_cmd('py-minimal')
+
+
+def test_script_doesnt_raise_if_arg_given(temp_file):
+    try:
+        run_cmd('py-minimal {}'.format(temp_file.name))
+    except CalledProcessError:
+        pytest.fail('Should not raise.')
 
 
 def run_cmd(cmd):
